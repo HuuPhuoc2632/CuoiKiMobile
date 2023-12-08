064247c02be0f53e0ab3811183663289ff529923
HuuPhuoc2632 <huuphuoc.2632@gmail.com>
2023-12-08 13:13:07 +0700

    hoan thanh cau 1

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Screen_01 from './screen/Screen_01';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Screen_02 from './screen/Screen_02';
+import { Provider } from 'react-redux';
+
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Screen_01" component={Screen_01} />
+        <Stack.Screen name="Screen_02" component={Screen_02} />
+      </Stack.Navigator>
+    </NavigationContainer>
+   
   );
 }
 
---
+++ screen/Screen_01.js
@@ -0,0 +1,51 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
+import { TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { useState } from 'react';
+
+export default function Screen_01() {
+    const [email, setEmail] = useState('abc@gmail.com');
+    const [password, setPassword] = useState('abcd1234');
+    const navigation = useNavigation();
+    const login = async () => {
+        fetch('https://6572ab32192318b7db407eff.mockapi.io/users', {
+  method: 'GET',
+  headers: {'content-type':'application/json'},
+}).then(async res => {
+  if (res.ok) {
+    const user = await res.json();
+    const currentUser = user.find(u => u.email === email && u.password === password);
+    if (currentUser) {
+        navigation.navigate('Screen_02');
+    } else {
+        alert('Wrong email or password');
+    }
+  }
+  // handle error
+}).then(tasks => {
+  // Do something with the list of tasks
+}).catch(error => {
+  // handle error
+})
+    }
+  return (
+    <View style={styles.container}>
+        <TextInput  style={{height: 40, borderColor: 'gray', borderWidth: 1, padding: 5, margin: 20}} placeholder='email' onChangeText={setEmail} value={email}/>
+        <TextInput  style={{height: 40, borderColor: 'gray', borderWidth: 1, padding: 5, margin: 20}} placeholder='password' onChangeText={setPassword} value={password}/>
+        <TouchableOpacity style={{height: 40, width: 100, backgroundColor: 'green', justifyContent:'center', alignItems:'center'}} onPress={login}>
+            <Text>Continue</Text>
+        </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+});
